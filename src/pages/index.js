ecaf07b2dd9b5eb4356e9894bbeb8b337e04d33e
Lucas Magalhães <lucas.magalhaes.cordeiro@usp.br>
2018-12-12 11:12:34 -0200

    :fire: Remove unnecessary file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Container, Form, Input, TextArea, Button, Dropdown } from 'semantic-ui-react'
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 
@@ -15,7 +14,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+      
     </div>
     <Link to="/page-2/"> Go to page 2 </Link>
     <Link to="/cinema_search/"> Cinema page </Link>
